f4b20ad6939af140c58b23965e7922cd0fc4a8d8
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-04-02 19:52:17 +0200

    Update users_mapper.py

--- models/users_mapper.py
+++ models/users_mapper.py
@@ -1,15 +1,20 @@
+import json
 from models.users import User
 
 
 
 # Konvertira User objekt u json i iz json formata kreira User objekt
 class UserMapper:
-
     def from_json(self, user_json: str) -> User:
         if isinstance(user_json, dict):
-            pass
+            return User(
+                id = user_json['id'],
+                name = user_json['name'],
+                email = user_json['email'],
+                phone = user_json['phone']
+            )
         else:
-            return User()
+            return None
 
-    def to_json(self) -> str:
-        pass
+    def to_json(self, user: User) -> str:
+        return json.dumps(user.__dict__)
